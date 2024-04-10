d311296d39e16d7eab8c5fcf87614fd567d2ce1a
manwarmeraj <125636985+manwarmeraj@users.noreply.github.com>
2024-04-10 16:44:12 +0530

    EmployeeDataget()

---
+++ Employee.Business/Contract/IEmployeeBusiness.cs
@@ -0,0 +1,14 @@
+﻿using Employee.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Business.Contract
+{
+    public interface IEmployeeBusiness
+    {
+        IEnumerable<EmployeeMaster> GetEmployee();
+    }
+}
---
+++ EmployeeData/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Employee.Business.Contract;
+using Employee.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeData.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeBusiness _EmployeeBusiness;
+        public EmployeeController(IEmployeeBusiness EmployeeBusiness)
+        {
+            _EmployeeBusiness = EmployeeBusiness;
+        }
+
+        [HttpGet()]
+        public IEnumerable<EmployeeMaster> Getemployee()
+        {
+            var result = _EmployeeBusiness.GetEmployee();
+            return result;
+        }
+    }
+}
